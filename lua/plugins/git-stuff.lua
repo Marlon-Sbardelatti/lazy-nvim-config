5e0206d796549ee27569631c758869c82e957051
Marlon-Sbardelatti <hetzwga@gmail.com>
2024-03-13 16:40:44 -0300

    Mar 13

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -10,13 +10,13 @@ return {
             {"<leader>gs", vim.cmd.Git},
         },
     },
-    -- {
-    --     "lewis6991/gitsigns.nvim",
-    --     config = function()
-    --         require("gitsigns").setup()
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
 
-    --         vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    --         vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    --     end,
-    -- },
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+        end,
+    },
 }
