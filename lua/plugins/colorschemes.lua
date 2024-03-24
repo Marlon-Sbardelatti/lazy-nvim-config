d19e4fe75c255c22f87c9ebe2476c9a24ca3aed9
Marlon-Sbardelatti <hetzwga@gmail.com>
2024-03-23 23:13:49 -0300

    toggleterm update

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,34 +1,40 @@
 return {
     {
-        "luisiacc/gruvbox-baby",
-		name = "gruvbox-baby"
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
     },
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		opts = {
-			transparent = true,
-		},
-		-- config = function()
-		-- 	vim.cmd.colorscheme("catppuccin")
-		-- end,
-	},
-	{
-		"tiagovla/tokyodark.nvim",
-		lazy = false,
-		name = "tokyodark",
-		priority = 1000,
-		opts = {
-			transparent = true,
-		},
-		config = function()
-			vim.cmd.colorscheme("tokyodark")
-		end,
-	},
-	{
-		"craftzdog/solarized-osaka.nvim",
-		opts = {
-			transparent = false,
-		},
-	},
+    {
+        "luisiacc/gruvbox-baby",
+        name = "gruvbox-baby"
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            transparent = true,
+        },
+        -- config = function()
+        -- 	vim.cmd.colorscheme("catppuccin")
+        -- end,
+    },
+    {
+        "tiagovla/tokyodark.nvim",
+        lazy = false,
+        name = "tokyodark",
+        priority = 1000,
+        opts = {
+            transparent = true,
+        },
+        config = function()
+            vim.cmd.colorscheme("tokyodark")
+        end,
+    },
+    {
+        "craftzdog/solarized-osaka.nvim",
+        opts = {
+            transparent = false,
+        },
+    },
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -10,7 +10,7 @@ return {
 			shading_factor = "1",
 			start_in_insert = true,
 			persist_size = true,
-			direction = "horizontal",
+			direction = "float",
 		},
 		keys = {
 			{ "<C-x>", ":ToggleTerm<CR>" },
