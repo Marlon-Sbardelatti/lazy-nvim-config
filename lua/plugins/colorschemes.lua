59968fd3751a7e8166ad06319e3d85a676c22904
Marlon-Sbardelatti <hetzwga@gmail.com>
2024-03-24 17:55:25 -0300

    Mar 24

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,36 +1,57 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		opts = {
-			transparent = true,
-		},
-		-- config = function()
-		-- 	vim.cmd.colorscheme("catppuccin")
-		-- end,
-	},
-	{
-		"tiagovla/tokyodark.nvim",
-		lazy = false,
-		name = "tokyodark",
-		priority = 1000,
-		opts = {
-			transparent = true,
-		},
-		config = function()
-			vim.cmd.colorscheme("tokyodark")
-		end,
-	},
-	{
-		"Shatur/neovim-ayu",
-	},
-	{
-		"Mofiqul/dracula.nvim",
-	},
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            style = "night",
+            styles = {
+                functions = {}
+            },
+            sidebars = { "qf", "vista_kind", "terminal", "packer" },
+        },
+        config = function(_, opts)
+            require("tokyonight").setup({
+                style = "night",
+                styles = {
+                    functions = {}
+                },
+                sidebars = { "qf", "vista_kind", "terminal", "packer" },
+                on_colors = function(colors)
+                    colors.bg = "#11121d"
+                    colors.bg_float = "#11121d"
+                    colors.fg_dark = "#A0A8CD"
+                end
+            })
+            -- vim.cmd.colorscheme("tokyonight-night")
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            transparent = true,
+        },
+        -- config = function()
+        -- 	vim.cmd.colorscheme("catppuccin")
+        -- end,
+    },
+    {
+        "tiagovla/tokyodark.nvim",
+        lazy = false,
+        name = "tokyodark",
+        priority = 1000,
+        opts = {
+            transparent = true,
+        },
+        config = function()
+            vim.cmd.colorscheme("tokyodark")
+        end,
+    },
+    {
+        "Shatur/neovim-ayu",
+    },
+    {
+        "Mofiqul/dracula.nvim",
+    },
 }
