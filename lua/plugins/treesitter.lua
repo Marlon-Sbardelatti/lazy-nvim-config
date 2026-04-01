4434d31e1f8d77f9dbf2c568964de8e135703ba7
Marlon-Sbardelatti <hetzwga@gmail.com>
2026-04-01 08:21:37 -0300

    treesiter its annoying

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,7 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
-        event = "BufReadPost",
-        build = ":TSUpdate",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
-
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            ["ii"] = "@conditional.inner",
-                            ["ai"] = "@conditional.outer",
-                            ["il"] = "@loop.inner",
-                            ["al"] = "@loop.outer",
-                        },
-                    },
-                },
-            })
-        end,
-    },
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate'
+    }
 }
