d97ca553764a188d8d5bc9601d34ec29e2e93c9c
Marlon-Sbardelatti <hetzwga@gmail.com>
2026-04-01 10:52:12 -0300

    tree-sitter makes me angry sometimes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,30 @@
 return {
     {
-        'nvim-treesitter/nvim-treesitter',
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
         lazy = false,
-        build = ':TSUpdate'
-    }
+        config = function()
+            vim.opt.termguicolors = true
+
+            local ft_to_parser = {
+                typescriptreact = "tsx",
+                javascriptreact = "tsx",
+                cs = "c_sharp",
+                sh = "bash",
+            }
+
+            vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+                callback = function(args)
+                    local ft = vim.bo[args.buf].filetype
+                    if ft == "" then return end
+
+                    local parser = ft_to_parser[ft] or ft
+
+                    pcall(vim.cmd, "TSInstall " .. parser)
+
+                    pcall(vim.treesitter.start, args.buf)
+                end,
+            })
+        end,
+    },
 }
