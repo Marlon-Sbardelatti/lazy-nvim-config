6cb6bd47e60b331e600acccc31dfb3ee2b663d73
Marlon-Sbardelatti <hetzwga@gmail.com>
2026-04-07 14:00:50 -0300

    add current lsp

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,50 +1,47 @@
 return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    -- lazy = true,
     config = function()
+        -- LSP status function
+        local function lsp_status()
+            local clients = vim.lsp.get_clients({
+                bufnr = vim.api.nvim_get_current_buf()
+            })
+
+            if next(clients) == nil then
+                return "No LSP"
+            end
+
+            local names = {}
+            for _, client in ipairs(clients) do
+                table.insert(names, client.name)
+            end
+
+            return table.concat(names, " ")
+        end
+
         require('lualine').setup {
             options = {
-                icons_enabled = true,
                 theme = 'auto',
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                disabled_filetypes = {
-                    statusline = {},
-                    winbar = {},
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                always_show_tabline = true,
-                globalstatus = false,
-                refresh = {
-                    statusline = 100,
-                    tabline = 100,
-                    winbar = 100,
-                }
             },
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype', function()
-                    return " " .. os.date("%R")
-                end },
+
+                lualine_x = {
+                    lsp_status,
+                    'encoding',
+                    'fileformat',
+                    'filetype',
+                    function()
+                        return " " .. os.date("%R")
+                    end
+                },
+
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
             },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { 'filename' },
-                lualine_x = { 'location' },
-                lualine_y = {},
-                lualine_z = {}
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {}
         }
     end,
 }
