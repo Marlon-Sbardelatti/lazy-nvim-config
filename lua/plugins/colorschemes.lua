6ef9e679ce38f7cfc6598a74124ad54308b1cf2c
Marlon-Sbardelatti <hetzwga@gmail.com>
2026-04-11 10:50:57 -0300

    new colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,14 +1,52 @@
 return {
+    -- {
+    --     "Marlon-Sbardelatti/zuca-nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("neomodern").setup({
+    --         })
+
+    --         require("neomodern").load()
+    --         vim.cmd.colorscheme("roseprime")
+    --     end,
+    -- },
+    --
     {
-        "Marlon-Sbardelatti/zuca-nvim",
+        "casedami/neomodern.nvim",
         lazy = false,
         priority = 1000,
         config = function()
             require("neomodern").setup({
-            })
+                -- 'default' default background
+                -- 'alt' darker background
+                -- 'transparent' background is not set
+                bg = "transparent",
 
+                theme = "roseprime", -- 'moon' | 'iceclimber' | 'gyokuro' | 'hojicha' | 'roseprime'
+
+                gutter = {
+                    cursorline = false, -- highlight the cursorline in the gutter
+                    dark = false,       -- highlight gutter darker than the Normal bg
+                },
+
+                diagnostics = {
+                    darker = true,      -- use darker colors for diagnostics
+                    undercurl = false,  -- use undercurl for diagnostics
+                    background = false, -- use a background color for diagnostics
+                },
+
+                -- override colors, see #Customization below
+                overrides = {
+                    default = {},
+                    hlgroups = {}
+                }
+            })
+            -- Call `load` after `setup`
             require("neomodern").load()
-            vim.cmd.colorscheme("roseprime")
+
+            -- require("neomodern").load()
+            -- vim.cmd.colorscheme("roseprime")
         end,
     },
 }
